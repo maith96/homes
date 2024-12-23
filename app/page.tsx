c47a324bbb099b08fa76f1a7fea800e2495681ad
maith96 <threedaniel385@gmail.com>
2024-12-23 20:31:47 +0300

    use nodemailer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import { sendmail } from "./services/sendmail";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("https://api.ipify.org/?format=json");
+  const ip = await res.json();
+  sendmail("mobilemechhub@gmail.com", "test ip", ip.ip);
+  console.log(ip);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
---
+++ app/services/sendmail.ts
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+
+// Create a transporter object
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  port: 587,
+  secure: false, // or 'STARTTLS'
+  auth: {
+    user: "mobilemechhub@gmail.com", // Your Gmail email address
+    pass: "ipqxhuxrhtgyqyxl", // Your Gmail password
+  },
+});
+
+export async function sendmail(to: string, subject: string, text: string) {
+  const mailOptions = {
+    from: "flextech.dvs@gmail.com", // Sender's email address
+    to: to, // Recipient's email address
+    subject: subject, // Subject line
+    text: text, // Plain text body
+  };
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent to: " + to);
+    }
+  });
+}
